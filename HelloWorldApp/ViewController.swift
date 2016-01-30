52de5b782c05bdad82f22367ac65cf4edde2b4ab
david <hamburger@davids-mac.local>
2016-01-30 14:16:46 -0600

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by david on 1/30/16.
+//  Copyright © 2016 david nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
