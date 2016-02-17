2a9e896222d92ba2e152cca6ebfc01ed645dfacb
David N <ddn051000@utdallas.edu>
2016-02-17 10:07:42 -0600

    committing all files

--- APRIMA EHR APP/ThirdViewController.swift
+++ APRIMA EHR APP/ThirdViewController.swift
@@ -1,9 +1,25 @@
 //
-//  ThirdViewController.swift
+//  FirstViewController.swift
 //  APRIMA EHR APP
 //
 //  Created by david on 2/17/16.
 //  Copyright © 2016 david nguyen. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
