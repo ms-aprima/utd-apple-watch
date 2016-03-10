c34e87db78dde003cc72457d67e047821db7c3d1
henry dinh <henryldinh@live.com>
2016-03-09 23:26:42 -0600

    Add profile image view and button in the Profile tab where the user can choose an image to use as the profile picture.

--- APRIMA EHR APP/ProfileViewController.swift
+++ APRIMA EHR APP/ProfileViewController.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     // initialize a HealthKit object to pull data from
     let health_kit: HealthKit = HealthKit()
     
     // View objects
-    
     @IBOutlet var table_view: UITableView!
+    @IBOutlet var profile_image_view: UIImageView!
+    @IBOutlet var profile_image_button: UIButton!
+    
+    var imagePicker = UIImagePickerController()
+    
+    // Lets the user update the profile image when the button is pressed.
+    // The button is pu on top of the image so that the user can just press the image to do this.
+    @IBAction func updateProfileImage(){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum){
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
     
     // Refreshes the UI
     func refreshUI(){
@@ -27,8 +41,14 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        // Set profile image radius to half the width of UIImageView to make it circular
+        profile_image_view.layer.cornerRadius = profile_image_view.frame.size.width / 2
+        profile_image_view.clipsToBounds = true
+        // Add a border to the image
+        profile_image_view.layer.borderWidth = 3.0
+        profile_image_view.layer.borderColor = UIColor.blackColor().CGColor
     }
     
     // Called everytime the UI is displayed (i.e. the user goes to the profile tab)
@@ -41,7 +61,12 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismissViewControllerAnimated(true, completion: { () -> Void in
+        })
+        profile_image_view.image = image
+    }
+
 
     /*
     // MARK: - Navigation
