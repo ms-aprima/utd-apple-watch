aed7e7fe55ddac91c00b4571f41f8de26f868105
David N <ddn051000@utdallas.edu>
2016-03-22 22:06:10 -0500

    added vital and heart rate views

---
+++ APRIMA EHR APP/VitalsVIewController.swift
@@ -0,0 +1,27 @@
+//
+//  VitalsVIewController.swift
+//  APRIMA EHR APP
+//
+//  Created by david on 3/22/16.
+//  Copyright © 2016 david nguyen. All rights reserved.
+//
+
+import UIKit
+
+class VitalsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+}
+}
